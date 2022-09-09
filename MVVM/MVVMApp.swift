6f2bcabc948a91816fe3d7d7b5c17b58c4330e4e
tkgka <ksh17171@gmail.com>
2022-09-09 15:48:39 +0900

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by 김수환 on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
